740c9adffa76981cef833400860c52557d6c336d
Dominik Schrempf <dominik.schrempf@gmail.com>
2025-04-21 14:48:21 +0200

    directives: more tests

--- test/Magix/DirectivesSpec.hs
+++ test/Magix/DirectivesSpec.hs
@@ -23,12 +23,12 @@ import Magix.Directives
     pMagixDirective,
     pShebang,
   )
-import Magix.Language (Language, getLanguageNameLowercase)
+import Magix.Language (Language (..), getLanguageNameLowercase)
 import Magix.Languages.Bash.Directives (BashDirectives (..))
 import Magix.Languages.Haskell.Directives (HaskellDirectives (..))
 import Magix.Languages.Python.Directives (PythonDirectives (..))
 import Magix.Tools (parseF, parseS)
-import Test.Hspec (Spec, describe, it, shouldBe)
+import Test.Hspec (Spec, SpecWith, describe, it, shouldBe)
 import Text.Megaparsec (parse)
 import Prelude hiding (readFile, unlines)
 
@@ -43,6 +43,36 @@ fnMinimalHaskell = "test-scripts/haskell/minimal"
 
 readMinimalHaskell :: IO Text
 readMinimalHaskell = readFile fnMinimalHaskell
+
+fnMinimalPython :: FilePath
+fnMinimalPython = "test-scripts/python/minimal"
+
+readMinimalPython :: IO Text
+readMinimalPython = readFile fnMinimalPython
+
+languages :: [Language]
+languages = [minBound .. maxBound]
+
+getEmptyDirectives :: Language -> Directives
+getEmptyDirectives Bash = BashD mempty
+getEmptyDirectives Haskell = HaskellD mempty
+getEmptyDirectives Python = PythonD mempty
+
+pEmptyLanguageDirectivesFor :: Language -> SpecWith ()
+pEmptyLanguageDirectivesFor language =
+  it description $
+    mapM_
+      testWith
+      [ "#!magix " <> name,
+        "#!magix " <> name <> "\t",
+        "#!magix \t" <> name <> "\t",
+        "#!magix \t" <> name <> "\n\n"
+      ]
+  where
+    description = "parses basic " <> show language <> " language directives"
+    name = getLanguageNameLowercase language
+    emptyDirectives = getEmptyDirectives language
+    testWith directives = parseS pLanguageDirectives directives emptyDirectives
 
 spec :: Spec
 spec = do
@@ -62,7 +92,7 @@ spec = do
     it "parses Magix directives" $ do
       let testLanguage lang =
             parseS pMagixDirective ("#!magix " <> getLanguageNameLowercase lang) lang
-      sequence_ [testLanguage lang | lang <- [minBound .. maxBound :: Language]]
+      sequence_ [testLanguage lang | lang <- languages]
 
     it "fails on wrong Magix directives" $ do
       parseF pMagixDirective "#!magic haskell"
@@ -79,29 +109,25 @@ spec = do
       parseF pLanguageDirectives "#!magix bash\n\n#!foo"
       parseF pLanguageDirectives "#!magix unknown"
 
+    mapM_ pEmptyLanguageDirectivesFor languages
+
+    -- Force testing of all languages.
     it "parses Bash language directives" $ do
-      let emptyBashDirectives = BashD $ BashDirectives []
-      parseS pLanguageDirectives "#!magix bash\n#!packages bar" $ BashD (BashDirectives ["bar"])
-      parseS pLanguageDirectives "#!magix bash" emptyBashDirectives
-      parseS pLanguageDirectives "#!magix bash\t" emptyBashDirectives
-      parseS pLanguageDirectives "#!magix \tbash\t" emptyBashDirectives
-      parseS pLanguageDirectives "#!magix \tbash\n\n" emptyBashDirectives
+      parseS pLanguageDirectives "#!magix bash\n#!packages bar" $
+        BashD (BashDirectives ["bar"])
 
     it "parses Haskell directives" $ do
-      let emptyHaskellDirectives = HaskellD $ HaskellDirectives [] []
-      parseS pLanguageDirectives "#!magix haskell" emptyHaskellDirectives
       parseS pLanguageDirectives "#!magix haskell\n#!ghcFlags -threaded" $
         HaskellD (HaskellDirectives [] ["-threaded"])
       parseS pLanguageDirectives "#!magix haskell\n#!haskellPackages bytestring\n#!ghcFlags -threaded" $
         HaskellD (HaskellDirectives ["bytestring"] ["-threaded"])
 
     it "parses Python directives" $ do
-      let emptyPythonDirectives = PythonD $ PythonDirectives []
-      parseS pLanguageDirectives "#!magix python" emptyPythonDirectives
       parseS pLanguageDirectives "#!magix python\n#!pythonPackages requests" $
         PythonD (PythonDirectives ["requests"])
 
   describe "pDirectives" $ do
+    -- Force testing of all languages.
     it "parses minimal sample scripts" $ do
       minimalBash <- readMinimalBash
       parse pDirectives fnMinimalBash minimalBash
@@ -109,6 +135,9 @@ spec = do
       minimalHaskell <- readMinimalHaskell
       parse pDirectives fnMinimalHaskell minimalHaskell
         `shouldBe` Right (HaskellD (HaskellDirectives ["bytestring"] ["-threaded"]))
+      minimalPython <- readMinimalPython
+      parse pDirectives fnMinimalPython minimalPython
+        `shouldBe` Right (PythonD (PythonDirectives ["numpy"]))
 
     it "parses some edge cases" $ do
       let spaceTest :: Text =
